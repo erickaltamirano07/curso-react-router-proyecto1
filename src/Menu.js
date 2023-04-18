608e632f9625f7f3ea93017719b7723513b14e57
Erick Altamirano <79813692+erickaltamirano07@users.noreply.github.com>
2023-04-18 18:15:30 -0500

    Link vs. NavLink
    clase 7

--- src/Menu.js
+++ src/Menu.js
@@ -1,7 +1,71 @@
 import React from "react";
+import { Link, NavLink, Route } from "react-router-dom";
 
 function Menu() {
-  return <nav>Menu</nav>;
+  return (
+    <nav>
+      <ul>
+        {/* <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>*/}
+        {/* <li>
+          <NavLink
+            to="/"
+            style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+            //className={({isActive}) => ''}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/blog"
+            style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+          >
+            Blog
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/profile"
+            style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+          >
+            Profile
+          </NavLink>
+        </li>*/}
+        {routes.map((route) => (
+          <li>
+            <NavLink
+              to={route.to}
+              style={({ isActive }) => ({ color: isActive ? "red" : "blue" })}
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
+
+const routes = [];
+routes.push({
+  to: "/",
+  text: "Home",
+});
+routes.push({
+  to: "/blog",
+  text: "Blog",
+});
+routes.push({
+  to: "/profile",
+  text: "Profile",
+});
 
 export { Menu };
