c63844aa3bc24a1ccd01aaf7ce1f2752f8d3e923
Erick Altamirano <79813692+erickaltamirano07@users.noreply.github.com>
2023-04-18 17:37:02 -0500

    Route: componentes de navegación
    Clase 6

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { BlogPage } from "./BlogPage";
+import { ProfilePage } from "./ProfilePage";
+import { Menu } from "./Menu";
+
+// /#/  --> HomePage
+// /#/blog   -->BlogPage
+// /#/profile  -->ProfilePage
+// /#/lalalal  -> NotFound
+// /blog, /lalala  --> Home
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <HashRouter>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<p>Not found</p>} />
+        </Routes>
+      </HashRouter>
+    </>
   );
 }
 
---
+++ src/BlogPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BlogPage() {
+  return <p>BlogPage</p>;
+}
+
+export { BlogPage };
