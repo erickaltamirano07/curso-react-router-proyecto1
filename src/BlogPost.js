b17a223d5d117c07f3dd289c3a4483dd90ac5d1d
Erick Altamirano <79813692+erickaltamirano07@users.noreply.github.com>
2023-04-19 12:06:49 -0500

    useNavigate: historial de navegación
    clase 9

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,14 +1,21 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "./blogdata";
 
 function BlogPost() {
+  const navigate = useNavigate();
   const { slug } = useParams();
   const blogpost = blogdata.find((post) => post.slug === slug);
+
+  const returnToBlog = () => {
+    navigate("/blog");
+    //navigate(-1);//ir para atras
+  };
 
   return (
     <>
       <h2>{blogpost.title}</h2>
+      <button onClick={returnToBlog}>Volver al Blog</button>
       <p>{blogpost.author}</p>
       <p>{blogpost.content}</p>
     </>
