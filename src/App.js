b0265628fc15a859d0eea2f78782d1399c6529e7
Erick Altamirano <79813692+erickaltamirano07@users.noreply.github.com>
2023-04-19 17:18:13 -0500

    useAuth: login y logout
    Clase 11

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./auth";
 import { HomePage } from "./HomePage";
 import { BlogPage } from "./BlogPage";
 import { ProfilePage } from "./ProfilePage";
 import { Menu } from "./Menu";
 import { BlogPost } from "./BlogPost";
-
-// /#/  --> HomePage
-// /#/blog   -->BlogPage
-// /#/profile  -->ProfilePage
-// /#/lalalal  -> NotFound
-// /blog, /lalala  --> Home
+import { LoginPage } from "./LoginPage";
+import { LogoutPage } from "./LogoutPage";
 
 function App() {
   return (
     <>
       <HashRouter>
-        <Menu />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
+        <AuthProvider>
+          <Menu />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
 
-          <Route path="/blog" element={<BlogPage />}>
-            <Route path=":slug" element={<BlogPost />} />
-          </Route>
+            <Route path="/blog" element={<BlogPage />}>
+              <Route path=":slug" element={<BlogPost />} />
+            </Route>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/logout" element={<LogoutPage />} />
 
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="*" element={<p>Not found</p>} />
-        </Routes>
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="*" element={<p>Not found</p>} />
+          </Routes>
+        </AuthProvider>
       </HashRouter>
     </>
   );
--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useAuth } from "./auth";
 
 function ProfilePage() {
-  return <p>ProfilePage</p>;
+  const auth = useAuth();
+  return (
+    <>
+      <p>ProfilePage</p>
+      <h1>{auth.user.username}</h1>
+    </>
+  );
 }
 
 export { ProfilePage };
